1b16c458c2f5b38219fb2e88d14268b071a6d306
Ye Haoze <yehaozee@gmail.com>
2025-01-18 11:50:58 +0100

    20250118 start

--- src/变量.js
+++ src/变量.js
@@ -6,6 +6,11 @@ export var 金币 = {
     数量: localStorage.getItem('金币数量') ? parseInt(localStorage.getItem('金币数量')) : 0,
 };
 
+export var 基地_升级 = {
+    升级炼金设备: localStorage.getItem('基地_升级_升级炼金设备') ? parseInt(localStorage.getItem('基地_升级_升级炼金设备')) : 0
+}
+
+
 export function 更新金币() {
         // 更新金币数量到localStorage
         localStorage.setItem('金币数量', 金币.数量);
@@ -14,8 +19,13 @@ export function 更新金币() {
         doc["炼金-金币"].innerHTML = '金币数量:' + 金币.数量;
 }
 
-export var 基地_升级 = {
-    升级炼金设备:localStorage.getItem('基地_升级_升级炼金设备') ? parseInt(localStorage.getItem('基地_升级_升级炼金设备')) : 0
+export function 更新基地_升级() {
+        // 更新金币数量到localStorage
+        localStorage.setItem('基地_升级_升级炼金设备', 基地_升级.升级炼金设备);
+
+
 }
+
+
 
 
